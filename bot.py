67d29f4971b91172dc2e6be09f4f83ef97c25561
Behzad Ravi <behzad015023@gmail.com>
2025-07-08 17:57:28 +0330

    Timezone setup conversation has been completed. Conversation end buttons are to be implemented.

--- bot.py
+++ bot.py
@@ -7,17 +7,17 @@ import logging
 # Telegram Bot related imports ->
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    ApplicationBuilder as AB, 
-    CommandHandler as CH, 
-    ContextTypes as CT, 
-    ConversationHandler as ConvoH, 
-    filters, 
-    MessageHandler as MH, 
+    ApplicationBuilder as AB,
+    CommandHandler as CH,
+    ContextTypes as CT,
+    ConversationHandler as ConvoH,
+    filters,
+    MessageHandler as MH,
     JobQueue as JQ)
 # Helper functions
 import helpers
 
-# State Definition for ConverstationHandlers
+# State Definition for ConversationHandlers
 GET_TIMEZONE_STATE = 1
 
 # Enable logging
@@ -31,27 +31,30 @@ async def start(update: Update, context: CT.DEFAULT_TYPE):
     user_id = update.effective_chat.id
 
     if not helpers.is_a_user(user_id):
-        await context.bot.send_message(chat_id=user_id, text="Welcome to TaskPrompt! Please enter your timezone (e.g. UTC+3:30).")
-        # Await users' timezone entery
+        await context.bot.send_message(chat_id=user_id,
+                                       text="Welcome to TaskPrompt! Please enter your IANA timezone (e.g. \"Asia/Tehran\" or \"Europe/Berlin\").")
+        # Await users' timezone entry
         return GET_TIMEZONE_STATE
     else:
-        await context.bot.sent_message(chat_id=user_id, text="What can I do for you today?")
+        await context.bot.send_message(chat_id=user_id, text="What can I do for you today?")
         # Don't start the conversation of getting the timezone
         return ConvoH.END
-    
+
 
 async def get_timezone(update: Update, context: CT.DEFAULT_TYPE):
     user_id = update.effective_chat.id
-    user_input = update.message
+    user_input = update.message.text
 
-    if not helpers.validate_timezone(user_input):
-        await context.bot.send_message(chat_id=user_id, text="That does not look like a valid UTC timezone. Use this format and try again (UTC+3:30).")
+    if not helpers.is_timezone_valid(user_input):
+        await context.bot.send_message(chat_id=user_id,
+                                       text="Doens't look like a valid IANA timezone. You could search for country's IANA and retry.")
         return GET_TIMEZONE_STATE
-    
+
     helpers.create_user_profile(user_id, user_input)
     logger.info(f"User {user_id} registered with timezone {user_input}")
 
-    await context.bot.send_message(chat_id=user_id, text=f"Your timezone has been set to {user_input}. What can I do for you today?")
+    await context.bot.send_message(chat_id=user_id,
+                                   text=f"Your timezone has been set to {user_input}. What can I do for you today?")
 
     return ConvoH.END
 
@@ -61,7 +64,8 @@ async def cancel(update: Update, context: CT.DEFAULT_TYPE):
 
     logging.info(f"User {user_id} cancelled the timezone setup.")
 
-    await context.bot.send_message(chat_id=user_id, text="You have cancelled timezone setup. You can always restart by tapping /start.")
+    await context.bot.send_message(chat_id=user_id,
+                                   text="You have cancelled timezone setup. You can always restart by tapping /start.")
 
     return ConvoH.END
 
@@ -72,9 +76,8 @@ if __name__ == "__main__":
     TOKEN = getenv("BOT_TOKEN")
     # Initiate the DB
     helpers.db_initiator()
-    
-    application = AB().token(TOKEN).build()
 
+    application = AB().token(TOKEN).build()
 
     setup_convo_handler = ConvoH(
         entry_points=[CH('start', start)],
@@ -85,9 +88,6 @@ if __name__ == "__main__":
         allow_reentry=True
     )
 
-
     application.add_handler(setup_convo_handler)
-    
-    application.run_polling()
-    
 
+    application.run_polling()
--- helpers.py
+++ helpers.py
@@ -1,30 +1,55 @@
 import sqlite3
+import pytz
+import datetime
+from datetime import datetime
 
 def execute_query(query, params=(), fetch=False):
     with sqlite3.connect("database.db") as conn:
-        cursor = conn.cursor
+        cursor = conn.cursor()
         cursor.execute(query, params)
-        return cursor.fetchall() if fetch else None
+        if fetch:
+            return cursor.fetchall()
+        else:
+            conn.commit()
+            return None 
     
 
 def db_initiator():
-    execute_query('''CREATE TABLE IF NOT EXISTS users(telegram_id INTEGER UNIQUE PRIMARY KEY, timezone TEXT, reminder_done_enabled BOOLEAN, reminder_left_enabled BOOLEAN);\
-                  CREATE TABLE IF NOT EXISTS tasks(id INTEGER UNIQUE PRIMARY KEY, user_id INTEGER, content TEXT, priority INTEGER, is_done BOOLEAN, create_at TIMESTAMP, FOREIGN KEY (user_id) REFERENCES users(telegram_id));\
-                  CREATE INDEX IF NOT EXISTS idx_telegram_id ON users(telegram_id);
-                  ''')
+    with sqlite3.connect("database.db") as conn:
+        conn.executescript('''
+            CREATE TABLE IF NOT EXISTS users(
+                telegram_id INTEGER UNIQUE PRIMARY KEY,
+                timezone TEXT,
+                reminder_done_enabled BOOLEAN,
+                reminder_left_enabled BOOLEAN
+            );
+            CREATE TABLE IF NOT EXISTS tasks(
+                id INTEGER UNIQUE PRIMARY KEY,
+                user_id INTEGER,
+                content TEXT,
+                priority INTEGER,
+                is_done BOOLEAN,
+                create_at TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(telegram_id)
+            );
+            CREATE INDEX IF NOT EXISTS idx_telegram_id ON users(telegram_id);
+        ''')
     
 
 def is_a_user(tid: str):
-    user = str(execute_query("SELECT * FROM users WHERE telegram_id = ?", params=(tid,), fetch=True))
+    user = execute_query('SELECT * FROM users WHERE telegram_id = ?', params=(tid,), fetch=True)
 
-    if user[0]:
+    if len(user) > 0:
         return True
     else:
         return False
 
+def is_timezone_valid(user_input: str) -> bool:
+    return (user_input in pytz.all_timezones_set)
 
-def validate_timezone(user_input):
-    pass
+def create_user_profile(tid, user_input):
+    user_tz = pytz.timezone(user_input)
+    user_tz_raw = datetime.now(user_tz).strftime("%z")
+    user_tz_offset = f"UTC{user_tz_raw[0:3]}:{user_tz_raw[3:]}"
 
-def create_user_profile(tid, tz):
-    pass
+    execute_query(query="INSERT INTO users VALUES (?, ?, ?, ?)", params=(tid, user_tz_offset, False, False))
